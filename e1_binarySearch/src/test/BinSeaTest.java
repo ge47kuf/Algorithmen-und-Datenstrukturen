f9b0e4070f2dccf2da237ca41cadc68521ae64f6
Tuan Le Anh <tuanla50@icloud.com>
2025-08-25 18:13:26 +0200

    change dir structure, add test files

---
+++ e1_binarySearch/src/test/BinSeaTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class BinSeaTest {
+    @Test
+    public void testBinarySearch() {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        assertEquals(0, 0);
+    }
+}
